1385810d7c660d03b4bc59975ba8f3ab2cc80ce1
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-06-01 00:14:56 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::middleware("auth")
     ->name("admin.")
     ->prefix("admin")
     ->group(function () {
-        Route::get("/", "HomeController@index")->name("home");
+        Route::get("/", "HomeController@index")->name("index");
         Route::resource("/posts", "PostController");
     });
 
