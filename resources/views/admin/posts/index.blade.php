880d2a85f6952df2ea75b8e8f431a23356ef8550
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-06-01 00:14:59 +0200

    Update index.blade.php

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -1,25 +1,45 @@
-@extends("layouts.app")
+@extends("layouts.dashboard")
 
 @section("content")
 
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('Dashboard') }}</div>
-
-                    <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
-
-                        {{ __('You are logged in!') }}
-                    </div>
-                </div>
-            </div>
-        </div>
+   <div class="row justify-content-between">
+    <div class="col-auto">
+        <h1>Tutti i posts</h1>
     </div>
+    <div class="col-auto">
+        <a href="{{ route('admin.posts.create') }}" class="btn btn-primary">Crea nuovo post</a>
+    </div>
+</div>
+<table class="table border-top border-bottom">
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>Titolo</th>
+            <th>Slug</th>
+            <th class="text/center">Azioni</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($posts as $post)
+            <tr>
+                <td>{{$post->id}}</td>
+                <td>{{$post->title}}</td>
+                <td>{{$post->slug}}</td>
+                <td>
+                    <a class="btn btn-info" href="{{ route("admin.posts.show", $post->id) }}">Dettaglio</a>
+                    <a class="btn btn-warning" href="{{ route("admin.posts.edit", $post->id) }}">Modifica</a>
+                    <form class="d-inline-block" action="{{route('admin.posts.destroy' ,  $post->id)}}" method="POST">
+                        @csrf
+                            @method('DELETE')
+
+                            <button class="btn btn-danger" onclick="return confirm('Are you sure you wanna delete the Post?');">
+                                Delete
+                            </button>
+                    </form>
+                </td>
+            </tr>
+        @endforeach
+    </tbody>
+</table>
 
 @endsection
