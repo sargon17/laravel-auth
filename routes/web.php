e1020c458cab66eee2dc647e07022b5941f562ec
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-05-31 12:51:26 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,20 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get("/", function () {
-//     return view("welcome");
-// });
-
 Auth::routes();
-
-// Route::get("/home", "HomeController@index")->name("home");
+// Auth::routes('register'=>false, 'reset'=>false, 'verify'=>false);  // disable registration
 
 Route::middleware("auth")
     ->namespace("Admin")
     ->name("admin.")
     ->prefix("admin")
     ->group(function () {
-        Route::get("/", "HomeController@index")->name("home");
+        Route::get("/", "HomeController@index")->name("index");
+        Route::resource("/posts", "PostController");
     });
 
 Route::get("{ant?}", function () {
